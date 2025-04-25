6cff3cfcdf5d894de412bb2a5415b055dfde114e
hjp845 <tlfgql@dgist.ac.kr>
2025-04-25 17:52:06 +0900

    네이버메타태크추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-// 폰트 설정
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -14,15 +13,16 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-// ✅ 사이트 제목 및 설명 + 구글 소유권 인증 메타태그
+// ✅ 사이트 제목 + 구글/네이버 메타태그 포함
 export const metadata: Metadata = {
   title: "날짜 계산기 사이트",
   description: "날짜 차이 계산, 특정한 날과 특정한 날까지의 날짜 차이 계산, 며칠 후 날짜 계산, 며칠 전 날짜 계산이 가능해요.",
   icons: {
-    icon: "/favicon-16x16.png",
+    icon: "/favicon.ico",
   },
   other: {
     "google-site-verification": "EoQ4-Bd1Mx2zZ1IJnhObp7lqwqD6No62hzZUh8Cxtk0",
+    "naver-site-verification": "8dc97fcdf04d84559cf0ee1cd50d4611c298301d"
   },
 };
 
