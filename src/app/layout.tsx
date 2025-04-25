2db0f46e2864d4fc128000c811167886a061fd6f
hjp845 <tlfgql@dgist.ac.kr>
2025-04-25 17:43:48 +0900

    구글seo태그삽입

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,12 +14,15 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-// ✅ 사이트 제목 및 설명 설정
+// ✅ 사이트 제목 및 설명 + 구글 소유권 인증 메타태그
 export const metadata: Metadata = {
   title: "날짜 계산기 사이트",
   description: "날짜 차이 계산, 특정한 날과 특정한 날까지의 날짜 차이 계산, 며칠 후 날짜 계산, 며칠 전 날짜 계산이 가능해요.",
   icons: {
-    icon: "/favicon.ico", // 또는 "/icon.png"
+    icon: "/favicon.ico",
+  },
+  other: {
+    "google-site-verification": "EoQ4-Bd1Mx2zZ1IJnhObp7lqwqD6No62hzZUh8Cxtk0",
   },
 };
 
